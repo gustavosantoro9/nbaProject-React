52a1cb163130408aa4f77aa9048b3ec23aff5b48
Gustavo <gustavosantoro.9@gmail.com>
2020-07-03 10:49:35 -0300

    pequenas correções

--- src/index.js
+++ src/index.js
@@ -2,59 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Inputs extends React.Component {
-	render(){
+class Form extends React.Component {
+	findPlayers() {
+		// <TO DO>
+	}
+
+	render() {
 		return (
 			<div className="fieldBox">
-				<label>Limite Salário</label>
+				<label>Salary Cap</label>
 				<input className="inputFields" type="number"></input>
-				<label>Limite Jogadores</label>
+				<label>Players Limit</label>
 				<input className="inputFields" type="number"></input>
-				<button>Search</button>
+				<button onClick={() => this.findPlayers()}>Search</button>
 			</div>
 		);
 	}
 }
 
 class Table extends React.Component {
-	render(){
-		return(
+	render() {
+		return (
 			<table>
-				<tr>
-					<th>Name</th>
-					<th>Points [avg]</th>
-					<th>Salary [per year]</th>
-				</tr>
-				<tr>
-					<td>Lebron James</td>
-					<td>28.7</td>
-					<td>37,600.000</td>
-				</tr>
-				<tr>
-					<td>Stephen Curry</td>
-					<td>27.7</td>
-					<td>38,600.000</td>
-				</tr>
-				<tr>
-					<td>Kawhi Leonard</td>
-					<td>25.4</td>
-					<td>32,000.000</td>
-				</tr>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Points [avg]</th>
+						<th>Salary [per year]</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Lebron James</td>
+						<td>28.7</td>
+						<td>37,600.000</td>
+					</tr>
+					<tr>
+						<td>Stephen Curry</td>
+						<td>27.7</td>
+						<td>38,600.000</td>
+					</tr>
+					<tr>
+						<td>Kawhi Leonard</td>
+						<td>25.4</td>
+						<td>32,000.000</td>
+					</tr>
+				</tbody>
 			</table>
 		);
 	}
 }
 
 class Menu extends React.Component {
-	render(){
-		return(
+	render() {
+		return (
 			<div>
 				<div className="box">
-				<h1>NBA Player Finder</h1>
-				<Inputs />
+					<h1>NBA Player Finder</h1>
+					<Form />
 				</div>
 				<div className="box">
-				<Table />
+					<Table />
 				</div>
 			</div>
 		);
